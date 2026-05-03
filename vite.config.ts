6356bb548030bffffc8b08b9c8e4a9483bb79487
viniciusnevescosta <vinicius.nevesco@gmail.com>
2026-05-03 12:57:15 -0300

    feat(release)!: Vite MPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "vite-plus";
+import Sitemap from "vite-plugin-sitemap";
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+// Recriando o __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   staged: {
@@ -6,4 +13,30 @@ export default defineConfig({
   },
   fmt: {},
   lint: { options: { typeAware: true, typeCheck: true } },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        community: resolve(__dirname, "community.html"),
+        donate: resolve(__dirname, "donate.html"),
+        faq: resolve(__dirname, "faq.html"),
+        news: resolve(__dirname, "news.html"),
+        projects: resolve(__dirname, "projects.html"),
+      },
+    },
+  },
+
+  plugins: [
+    Sitemap({
+      hostname: "https://loosebird.org",
+      dynamicRoutes: ["/", "/community", "/donate", "/faq", "/news", "/projects"],
+      generateRobotsTxt: true,
+      robots: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+    }),
+  ],
 });
